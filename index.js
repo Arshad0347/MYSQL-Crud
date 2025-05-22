f3047f009d781ac29fd4c3ac343e96cf7209ec18
Arshad0347 <arshad.eqbal009@gmail.com>
2025-05-22 14:03:45 +0530

    AddUser to Database

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
+PORT=9000;
 const express=require('express');
 const app=express();
+const database=require('./modules/connections');
+app.use(express.json());
 
-PORT=9000;
+app.post('/AddUser',(req,res)=>{
+    const data={
+        name:req.body.name,
+        rollno:req.body.rollno,
+        email:req.body.email,
+        phone:req.body.mobile
+    }
+    // console.log(data);
+    const sql="INSERT INTO `studentsdata` SET ?";
+    database.query(sql,data,(error,result)=>{
+        if(error)console.log(error.sqlMessage);
+        else res.json()
+    })
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT,()=>{
     console.log(`server started ${PORT}`);
